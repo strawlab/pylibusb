72d9f17977913d6ef10ecb2bcd95c564dad7eb0c
astraw <astraw@2e4ac0da-de22-0410-b3ca-d3f924e22bd5>
2007-03-06 23:57:03 +0000

    update to register on PyPI
    git-svn-id: https://code.astraw.com/pylibusb/trunk@6 2e4ac0da-de22-0410-b3ca-d3f924e22bd5

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup
 
 setup(name='pylibusb',
       description='libusb wrapper (ctypes based)',
+      url='https://code.astraw.com/projects/pylibusb',
       version='0.1',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
       packages = ['pylibusb'],
+      install_requires = ['ctypes'],
       zip_safe=True)
