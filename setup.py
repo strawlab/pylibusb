50f621cdb58d24205ba09b0209cbeb57a94bf388
astraw <astraw@2e4ac0da-de22-0410-b3ca-d3f924e22bd5>
2009-03-29 19:16:15 +0000

    bump version for release, remove zip_safe setup option
    git-svn-id: https://code.astraw.com/pylibusb/trunk@11 2e4ac0da-de22-0410-b3ca-d3f924e22bd5

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='pylibusb',
       description='libusb wrapper (ctypes based)',
       url='https://code.astraw.com/projects/pylibusb',
-      version='0.1',
+      version='0.2',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
       license='BSD',
       packages = ['pylibusb'],
-      zip_safe=True)
+      )
